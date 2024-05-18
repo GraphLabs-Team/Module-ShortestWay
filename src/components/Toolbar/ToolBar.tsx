1af461e1ce174202108ccf5e710f8972ed2161f8
AnutaWest <ane4karpova2003@gmail.com>
2024-05-18 13:33:39 +0300

    [refactor: adding a new button]

--- src/components/Toolbar/ToolBar.tsx
+++ src/components/Toolbar/ToolBar.tsx
@@ -45,6 +45,7 @@ export class ToolBar extends React.Component<IToolBarProps> {
             button_list.push(<input type="color" id="nodeColor" className="colorInput" name="nodeColor"/>)
             button_list.push(<button id="recolorNodeButton" className={"toolButton"} type="button">Раскрасить вершину</button>)
             button_list.push(<button id="recolorEdgeButton" className={"toolButton"} type="button">Раскрасить ребро</button>)
+            button_list.push(<button id="uncolorEdgeButton" className={"toolButton"} type="button">Снять окрашивание с ребра</button>)
         }
         if (this.props.graph_adj_coloring_buttons){
             button_list.push(<button id="colorAdjNodeButton" className={"toolButton"} type="button">Выделить смежные вершины</button>)
